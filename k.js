9a60d7877769017a5faf70dd4263f9142ba3af71
Anderson Celso <64502987+andersondiniz1@users.noreply.github.com>
2023-09-12 12:43:28 +0000

    k1

---
+++ k.js
@@ -0,0 +1,36 @@
+// const aluno = {};
+//     aluno.nome = "Paulo";
+//         aluno.saudacao = function () {
+//             let sobrenome = "Costa"
+//             console.log("Ola " + this.nome + " " + this.sobrenome)
+//             };
+// console.log(aluno);
+// aluno.saudacao();
+
+// const pessoa = {
+//     nome: "Anderson"
+// };
+// console.log(pessoa.nome); // Anderosn
+// let aluno = pessoa;
+// aluno.nome = "João";
+// console.log(pessoa.nome); // João
+
+// function pessoa() {
+//     this.nome = "Anderson"
+// };
+// const pessoa1 = new pessoa();
+// pessoa1.idade = 25;
+
+// function pessoa (nome_pessoa, idade_pessoa, genero_pessoa) {
+//     this.nome = nome_pessoa,
+//     this.idade = idade_pessoa,
+//     this.genero = genero_pessoa,
+//     this.saudacao = function () {
+//         return("Oi " + this.nome);
+//     }
+// };
+// const pessoa1 = new pessoa("Pedro", 23, "Masculino");
+// const pessoa2 = new pessoa("João", 27, "Masculino");
+// console.log(pessoa1.nome);
+// console.log(pessoa2.nome);
+
