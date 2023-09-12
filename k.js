e389eb3a460acbe9dc02254125bc78ab6cd7739e
Anderson Celso <64502987+andersondiniz1@users.noreply.github.com>
2023-09-12 14:36:39 +0000

    k2

--- k.js
+++ k.js
@@ -34,3 +34,72 @@
 // console.log(pessoa1.nome);
 // console.log(pessoa2.nome);
 
+// const aluno = {
+//     nome: "Pedro",
+//     get getNome() {
+//         return this.nome;
+//     },
+// };
+// console.log(aluno.nome);
+// console.log(aluno.getnome);
+// console.log(aluno.getNome());
+
+// const aluno = {
+//     nome: "Pedro",
+//     set setNome(novo_nome) {
+//         this.nome = novo_nome;
+//     },
+// };
+// aluno.setNome = "Maria";
+// console.log(aluno.nome);
+
+///////////////////////// CLASSES /////////////////////////
+
+// class pessoa {
+//     constructor(nome, idade){
+//         this.nome = nome;
+//         this.idade = idade;
+//     }
+//     saudacao(texto) {
+//         console.log(`Olá! ${texto}`)
+//     }
+// }
+// const p1 = new pessoa("Anderson", 22);
+// console.log(p1)
+// p1.saudacao("Ana");
+
+///////////////////////// HOISTING /////////////////////////
+
+// console.log(soma(3, 5))
+// function soma(n1, n2) {
+//     return n1 + n2;
+// }
+
+///////////////////////// HERANÇAS /////////////////////////
+
+// class pessoa() {
+//     constructor(nome) {
+//         this.nome = nome;
+//     }
+// }
+// class aluno extends pessoa {
+//     constructor(nome) {
+//         super(nome);
+//         this.idade = idade;
+//     }
+//     saudacao(){
+//         console.log("Olá Aluno!")
+//     }
+// }
+//     saudacao(nome){
+//         console.log("Olá Aluno!")
+// }
+// }
+// let aluno1 = new aluno("Anderson", 26);
+// let aluno2 = new aluno("Paulo", 30)
+// let listaAlunos = [];
+// listaAlunos.push(aluno1);
+// listaAlunos.push(aluno2);
+// aluno1.saudacao();
+// aluno1.saudacao("Anderson");
+// aluno1.saudacao();
