5860a992ec1f524b0057edcaa0989a11422d5731
Anderson Ribeiro <64502987+andersondiniz1@users.noreply.github.com>
2023-12-14 01:38:34 +0000

    Desafio2

--- meu-servidor/Desafio2.js
+++ meu-servidor/Desafio2.js
@@ -1,17 +1,27 @@
+// Saudação Pernozalizada: https://ideal-fortnight-rw4gjwr95g53wjx7-3002.app.github.dev/saudacao/nomeQdeseja
+// Usuarios: https://ideal-fortnight-rw4gjwr95g53wjx7-3002.app.github.dev/usuarios
+// Usuarios 1: https://ideal-fortnight-rw4gjwr95g53wjx7-3002.app.github.dev/usuarios/1
+// Usuarios 2: https://ideal-fortnight-rw4gjwr95g53wjx7-3002.app.github.dev/usuarios/2
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const porta = 3000;
+const porta = 3002;
+
+// Simulação de uma lista de usuários (pode ser substituída por um banco de dados)
+const usuarios = [
+    { id: 1, nome: 'Isaac' },
+    { id: 2, nome: 'Egito' },
+];
 
 // Adicionando o middleware BodyParser para processar dados do corpo da solicitação
 app.use(bodyParser.json());
 
-// Lista de usuários fictícia (pode ser substituída por um banco de dados)
-const usuarios = [
-    { id: 1, nome: 'Usuário 1' },
-    { id: 2, nome: 'Usuário 2' },
-];
+// Rota para a raiz ("/")
+app.get('/', (req, res) => {
+    res.send('Bem-vindo à página inicial do meu web service!');
+});
 
 // Rota para listar todos os usuários
 app.get('/usuarios', (req, res) => {
@@ -35,7 +45,6 @@ app.post('/usuarios', (req, res) => {
     const novoUsuario = req.body;
 
     if (novoUsuario && novoUsuario.nome) {
-        // Simulação de um ID único (pode ser substituído por um mecanismo mais robusto)
         novoUsuario.id = usuarios.length + 1;
         usuarios.push(novoUsuario);
 
@@ -45,8 +54,23 @@ app.post('/usuarios', (req, res) => {
     }
 });
 
-// Iniciando o servidor
-app.listen(porta, () => {
-    console.log(`Servidor rodando em http://localhost:${porta}...`);
+
+// Rota para saudação personalizada
+app.get('/saudacao/:nome', (req, res) => {
+    const nome = req.params.nome;
+    res.json({ mensagem: `Olá, ${nome}! Bem-vindo ao meu web service!` });
 });
 
+// Rota para lidar com o erro 404
+app.use((req, res) => {
+    res.status(404).json({ mensagem: 'Erro 404 - Página não encontrada' });
+});
+
+// Iniciando o servidor
+app.listen(porta, () => {
+    console.log(`Servidor rodando em http://localhost:${porta}`);
+    console.log(`Para listar todos os usuários: http://localhost:${porta}/usuarios`);
+    console.log(`Para obter detalhes de um usuário: http://localhost:${porta}/usuarios/{ID}`);
+    console.log(`Para adicionar um novo usuário: http://localhost:${porta}/usuarios (método POST)`);
+    console.log(`Para uma saudação personalizada: http://localhost:${porta}/saudacao/{SeuNome}`);
+});
